79cdeed0feea2bfdad223bfeb7cf2c2de3c17128
yurynefedov <yury.a.nefedov@gmail.com>
2023-03-02 17:43:48 +0300

    feat: add locales in russian

---
+++ src/locales/ru.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    inputFeedback: {
+      notValidUrl: 'Ссылка должна быть валидным URL',
+      alreadyExist: 'RSS уже существует',
+      notValidRss: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
+      unknownError: 'Что-то пошло не так',
+      success: 'RSS успешно загружен',
+    },
+  },
+};
